9a4a3f3c7cdbc6856918ef22c7f05183ea8501c0
nihil39 <nihil39@gmail.com>
2021-08-06 18:05:59 +0200

    Prova ssh

--- Script_bash/script_rinominazione_preliminare.sh
+++ Script_bash/script_rinominazione_preliminare.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-#Questo script toglie le righe inutili dal file su cui sono salvate le configurazioni generate da lammps e lo rinomina. Da lanciare prima dei run in cui si calcolano le 20 velocita' per ogni configurazione
-
+#Questo script toglie le righe inutili dal file su cui sono salvate le configurazioni generate da lammps e lo rinomina. Da lanciare prima dei run in cui si calcolano le 20 velocita' per ogni configurazione. github
 for i in {003..400}; do
 	pushd $i
 	sed '14,18d' conf_equil_lammps_T044_P293_random_seed_file > conf_equil_T044_P293
