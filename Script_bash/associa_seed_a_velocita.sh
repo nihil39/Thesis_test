4b64ea339c5cb04e71aa1377fc551f3c3bff67c1
nihil39 <nihil39@gmail.com>
2021-08-25 10:31:39 +0200

    Added .py NN file

--- Script_bash/associa_seed_a_velocita.sh
+++ Script_bash/associa_seed_a_velocita.sh
@@ -4,8 +4,7 @@ for i in {400..400};
 
 do	
 	pushd $i
-	rm *MB*
 	mpirun -np 12 --use-hwthread-cpus /home/linuxbrew/.linuxbrew/bin/lmp_mpi -in ../run_recupero_velocita.lammps > ./out_recupero_velocita
-	
+    #Server mpirun -np 64 --use-hwthread-cpus lmp < ../mio_nve.lammps > ./out_nve_T044_P293_run_${k}_seed-${seed}	
 	popd
 done
