977be0f1765f38c2ace0ead35079de3dfd90fc00
nihil39 <nihil39@gmail.com>
2021-11-02 12:19:48 +0100

    Add files via upload

--- Script_python/prova_fit_istogrammi.py
+++ Script_python/prova_fit_istogrammi.py
@@ -5,37 +5,37 @@ from scipy import stats
 
 predizioni_T050 = np.load('predictions_k20_e30_bs16_cpMax_T050.npy')
 
+low_threshold = 4.07
+
 fig = plt.figure()
 
-#fig.suptitle('Prediction on the single conf, DGCNN Trained on all conf\n Density = True, t = 199', fontsize = 14, c='darkgrey')
-ax1 = fig.add_subplot(1, 1, 1)
+fig.suptitle(f'T = 0.50, P = 1.55\nexcluding values < {low_threshold}', fontsize = 14, c='darkgrey')
+ax1 = fig.add_subplot(1, 2, 1)
+ax2 = fig.add_subplot(1, 2, 2)
+n, bins_T050, patches = ax1.hist(predizioni_T050[:,1], bins = 'auto',  color = 'orange',  ec = 'skyblue', density = True)
 
-#n, bins_T050, patches = ax1.hist(predizioni_T050[:,1], bins = 'auto',  color = 'orange',  ec = 'skyblue')
-
-#np.where(predizioni_T050[:,1] < 4) # trova gli indici degli elementi nella colonna [:,1] che hanno valore < 4
-nuove_predizioni_T050_t199 = np.delete(predizioni_T050[:,1], np.argwhere(predizioni_T050[:,1] < 4.05 ))
-
-n, bins_T050_t199_nuova, patches = ax1.hist(nuove_predizioni_T050_t199, bins = 'auto',  color = 'orange',  ec = 'skyblue', density = True)
+#np.where(predizioni_T050[:,1] < low_threshold)  trova gli indici degli elementi nella colonna [:,1] che hanno valore < low_threshold
+nuove_predizioni_T050_t199 = np.delete(predizioni_T050[:,1], np.argwhere(predizioni_T050[:,1] < low_threshold ))
+n, bins_T050_t199_nuova, patches = ax2.hist(nuove_predizioni_T050_t199, bins = 'auto',  color = 'orange',  ec = 'skyblue', density = True)
 
 #n, bins_T050 = np.histogram(predizioni_T050[:,1], bins = 'auto')
-#mu_T050, sigma_T050 = sp.stats.norm.fit(predizioni_T050[:,1])
+mu_T050, sigma_T050 = sp.stats.norm.fit(predizioni_T050[:,1])
 
 mu_T050_t199_nuova, sigma_T050_t199_nuova = sp.stats.norm.fit(nuove_predizioni_T050_t199)
-#cv_T050 = sigma_T050 / abs(mu_T050)
+cv_T050 = sigma_T050 / abs(mu_T050)
 
 cv_T050_t199_nuova = sigma_T050_t199_nuova / abs(mu_T050_t199_nuova)
-#best_fit_line_T050 = sp.stats.norm.pdf(bins_T050, mu_T050, sigma_T050) # (, media, varianza)
-#best_fit_line_T050 = sp.stats.norm.pdf(bins_T050, 4.3, sigma_T050)
-    
-best_fit_line_T050_t199_nuova = sp.stats.norm.pdf(bins_T050_t199_nuova, mu_T050_t199_nuova, sigma_T050_t199_nuova) # (, media, varianza)
+best_fit_line_T050 = sp.stats.norm.pdf(bins_T050, mu_T050, sigma_T050) # (, media, varianza)
+
+best_fit_line_T050_t199_nuova = sp.stats.norm.pdf(bins_T050_t199_nuova, mu_T050_t199_nuova, sigma_T050_t199_nuova)
 
 #ax1.set_title(f'T = 0.50 P = 1.55')
-#ax1.text(0.1, 0.8, f'μ = {mu_T050:.4f}\nσ = {sigma_T050:.4f}\ncv = {cv_T050:.3f}', fontsize = 10,  transform = ax1.transAxes)
 
-#ax1.plot(bins_T050, best_fit_line_T050)
+ax1.text(0.1, 0.8, f'μ = {mu_T050:.4f}\nσ = {sigma_T050:.4f}\ncv = {cv_T050:.3f}', fontsize = 10,  transform = ax1.transAxes)
+ax2.text(0.1, 0.8, f'μ_nuova = {mu_T050_t199_nuova:.4f}\nσ_nuova = {sigma_T050_t199_nuova:.4f}\ncv_nuova = {cv_T050_t199_nuova:.3f}', fontsize = 10,  transform = ax2.transAxes)
 
-ax1.plot(bins_T050_t199_nuova, best_fit_line_T050_t199_nuova)
-#ax1.plot(predizioni_T050[:,1], bins)
+ax1.plot(bins_T050, best_fit_line_T050)
+ax2.plot(bins_T050_t199_nuova, best_fit_line_T050_t199_nuova)
 
 #plt.hist(predizioni_T050[:,1], bins = range(4, 5), density = True)
 #plt.xlim(4,4.6)
@@ -49,6 +49,6 @@ ax1.plot(bins_T050_t199_nuova, best_fit_line_T050_t199_nuova)
 #plt.bar(center, n, align='center', width=width)
 
 plt.show()
-
+plt.cla()
 
 
