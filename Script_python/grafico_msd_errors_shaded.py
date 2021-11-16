79a4d301b080240422811418bb5592dfb9ad407c
nihil39 <nihil39@gmail.com>
2021-11-16 21:17:28 +0100

    Add files via upload

---
+++ Script_python/grafico_msd_errors_shaded.py
@@ -0,0 +1,56 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+array_mu_sigma_T044 = np.load('array_mu_sigma_T044.npy')
+array_mu_sigma_T045_NT = np.load('array_mu_sigma_T045_NT.npy')
+array_mu_sigma_T047 = np.load('array_mu_sigma_T047.npy')
+array_mu_sigma_T049_NT = np.load('array_mu_sigma_T049_NT.npy')
+array_mu_sigma_T050 = np.load('array_mu_sigma_T050.npy')
+array_mu_sigma_T052_NT = np.load('array_mu_sigma_T052_NT.npy')
+array_mu_sigma_T056 = np.load('array_mu_sigma_T056.npy')
+#array_mu_sigma syntax [tempi, (0: timestep, 1: media partA, 2: sigma partA, 3: media partB, 4: sigma partB]
+
+error_T044 = array_mu_sigma_T044[:, 2] 
+error_T045_NT = array_mu_sigma_T045_NT[:, 2] 
+error_T047 = array_mu_sigma_T047[:, 2] 
+error_T049_NT = array_mu_sigma_T049_NT[:, 2] 
+error_T050 = array_mu_sigma_T050[:, 2] 
+error_T052_NT = array_mu_sigma_T052_NT[:, 2] 
+error_T056 = array_mu_sigma_T056[:, 2] 
+
+fig = plt.figure()
+fig.suptitle('MSD with shaded error region', fontsize = 14, c = 'darkgrey')
+
+ax2 = fig.add_subplot(1, 1, 1)
+
+ax2.plot(array_mu_sigma_T056[:, 0], array_mu_sigma_T056[:, 1], color = 'tab:red', label = "T = 0.56, P = 0.17" ) #ax1.scatter per lo scatter plot
+ax2.fill_between(array_mu_sigma_T056[:, 0], array_mu_sigma_T056[:, 1] + error_T056, array_mu_sigma_T056[:, 1] - error_T056, color = 'indianred',linestyle = 'dashdot', edgecolor = 'slategrey') #ax1.scatter per lo scatter plot
+
+ax2.plot(array_mu_sigma_T052_NT[:, 0], array_mu_sigma_T052_NT[:, 1], color = 'coral', label = "T = 0.52, P = 1.09 NT" ) 
+ax2.fill_between(array_mu_sigma_T052_NT[:, 0], array_mu_sigma_T052_NT[:, 1] + error_T052_NT, array_mu_sigma_T052_NT[:, 1] - error_T052_NT, color = 'mistyrose',linestyle = 'dashdot', edgecolor = 'slategrey') 
+
+ax2.plot(array_mu_sigma_T050[:, 0], array_mu_sigma_T050[:, 1], color = 'orange', label = "T = 0.50, P = 1.55" ) 
+ax2.fill_between(array_mu_sigma_T050[:, 0], array_mu_sigma_T050[:, 1] + error_T050, array_mu_sigma_T050[:, 1] - error_T050, color = 'wheat',linestyle = 'dashdot', edgecolor = 'slategrey') 
+
+ax2.plot(array_mu_sigma_T049_NT[:, 0], array_mu_sigma_T049_NT[:, 1], color = 'gold', label = "T = 0.49, P = 1.78 NT" )
+ax2.fill_between(array_mu_sigma_T049_NT[:, 0], array_mu_sigma_T049_NT[:, 1] + error_T049_NT, array_mu_sigma_T049_NT[:, 1] - error_T049_NT, color = 'xkcd:cream',linestyle = 'dashdot', edgecolor = 'slategrey') 
+
+ax2.plot(array_mu_sigma_T047[:, 0], array_mu_sigma_T047[:, 1], color = 'mediumseagreen', label = "T = 0.47, P = 2.24" ) 
+ax2.fill_between(array_mu_sigma_T047[:, 0], array_mu_sigma_T047[:, 1] + error_T047, array_mu_sigma_T047[:, 1] - error_T047, color = 'mediumspringgreen',linestyle = 'dashdot',  edgecolor = 'slategrey') 
+
+ax2.plot(array_mu_sigma_T045_NT[:, 0], array_mu_sigma_T045_NT[:, 1], color = 'cornflowerblue', label = "T = 0.45, P = 2.70 NT" ) 
+ax2.fill_between(array_mu_sigma_T045_NT[:, 0], array_mu_sigma_T045_NT[:, 1] + error_T045_NT, array_mu_sigma_T045_NT[:, 1] - error_T045_NT, color = 'lightsteelblue',  linestyle = 'dashdot', edgecolor = 'slategrey')
+
+ax2.plot(array_mu_sigma_T044[:, 0], array_mu_sigma_T044[:, 1], color = 'tab:blue', label = "T = 0.44, P = 2.93" )
+ax2.fill_between(array_mu_sigma_T044[:, 0], array_mu_sigma_T044[:, 1] + error_T044, array_mu_sigma_T044[:, 1] - error_T044, color = 'lavender', linestyle = 'dashdot', edgecolor = 'slategrey')
+
+ax2.legend()
+ax2.set_xlabel('time (#iterations)')
+ax2.set_ylabel(r'$\langle r^2(t) \rangle$', fontsize = 14) #r' ' per scrivere in latex tra gli apici
+ax2.set_xscale('log')
+ax2.set_yscale('log')
+
+ax2.grid(linestyle = '--', linewidth = 0.5)
+
+plt.show()
+
--- Script_python/grafico_msd_uniti_senza_prediction.py
+++ Script_python/grafico_msd_uniti_senza_prediction.py
@@ -12,15 +12,11 @@ from scipy import stats
 #array_msd_T056 = np.load('msd_completo_T056_P017.npy') #[configurazioni, (questo campo deve scorrere coi :),  0 = tempi / 1 = particle_type / 2 = msdB]
 
 array_msd_T044_medie = np.load('msd_T044_P293_medie.npy')
-
 array_msd_T045_NT_medie = np.load('msd_T045_P270_NT_medie.npy')
-
 array_msd_T047_medie = np.load('msd_T047_P224_medie.npy')
-
+array_msd_T049_NT_medie = np.load('msd_T049_P178_NT_medie.npy')
 array_msd_T050_medie = np.load('msd_T050_P155_medie.npy')
-
 array_msd_T052_NT_medie = np.load('msd_T052_P109_NT_medie.npy') #[configurazioni, (questo campo deve scorrere coi :),  0 = tempi / 1 = particle_type / 2 = msdB]
-
 array_msd_T056_medie = np.load('msd_T056_P017_medie.npy') #[configurazioni, (questo campo deve scorrere coi :),  0 = tempi / 1 = msdA / 2 = msdB]
 
 particle_type = 1 # 1 = A, 2 = B
@@ -43,12 +39,13 @@ configurazione = random.randint(0, 7999)
 #ax1.plot(array_msd_T047[0, :, 0], array_msd_T050[configurazione, :, particle_type], color = 'orange', label = "T = 0.50, P = 1.55")
 #ax1.plot(array_msd_T047[0, :, 0], array_msd_T056[configurazione, :, particle_type], color = 'tab:red', label = "T = 0.56, P = 0.17")
 
-ax2.plot(array_msd_T044_medie[:, 0], array_msd_T044_medie[:, particle_type], color = 'tab:blue', label = "T = 0.44, P = 2.93" ) #ax1.scatter per lo scatter plot
-ax2.plot(array_msd_T045_NT_medie[:, 0], array_msd_T045_NT_medie[:, particle_type], color = 'cornflowerblue', linestyle = 'dashed', label = "T = 0.45, P = 2.70 NT" ) #ax1.scatter per lo scatter plot
-ax2.plot(array_msd_T047_medie[:, 0], array_msd_T047_medie[:, particle_type], color = 'mediumseagreen', label = "T = 0.47, P = 2.24" )
-ax2.plot(array_msd_T050_medie[:, 0], array_msd_T050_medie[:, particle_type], color = 'orange', label = "T = 0.50, P = 1.55")
-ax2.plot(array_msd_T052_NT_medie[:, 0], array_msd_T052_NT_medie[:, particle_type], color = 'coral', linestyle = 'dashed', label = "T = 0.52, P = 1.09 NT")
 ax2.plot(array_msd_T056_medie[:, 0], array_msd_T056_medie[:, particle_type], color = 'tab:red', label = "T = 0.56, P = 0.17")
+ax2.plot(array_msd_T052_NT_medie[:, 0], array_msd_T052_NT_medie[:, particle_type], color = 'coral', linestyle = 'dashed', label = "T = 0.52, P = 1.09 NT")
+ax2.plot(array_msd_T050_medie[:, 0], array_msd_T050_medie[:, particle_type], color = 'orange', label = "T = 0.50, P = 1.55")
+ax2.plot(array_msd_T049_NT_medie[:, 0], array_msd_T049_NT_medie[:, particle_type], color = 'gold', linestyle = 'dashed', label = "T = 0.49, P = 1.78 NT" )
+ax2.plot(array_msd_T047_medie[:, 0], array_msd_T047_medie[:, particle_type], color = 'mediumseagreen', label = "T = 0.47, P = 2.24" )
+ax2.plot(array_msd_T045_NT_medie[:, 0], array_msd_T045_NT_medie[:, particle_type], color = 'cornflowerblue', linestyle = 'dashed', label = "T = 0.45, P = 2.70 NT" ) #ax1.scatter per lo scatter plot
+ax2.plot(array_msd_T044_medie[:, 0], array_msd_T044_medie[:, particle_type], color = 'tab:blue', label = "T = 0.44, P = 2.93" ) #ax1.scatter per lo scatter plot
 
 
 #ax1.set_title(f'One configuration')
